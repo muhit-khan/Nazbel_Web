44e1007b267cb4af92ab12146a3a4586d7eb5357
muhit-khan <muhitkhan_bd@outlook.com>
2024-01-03 21:22:42 +0530

    Add services data to Homepage body Services

---
+++ src/pages/Homepage/body/Services/data.js
@@ -0,0 +1,16 @@
+const servicesData = [
+    {
+        id: 1,
+        name: "Abc"
+    },
+    {
+        id: 2,
+        name: "Bcd"
+    },
+    {
+        id: 3,
+        name: "Cde"
+    },
+]
+
+export { servicesData }
