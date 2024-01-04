cb85b5261a7a8a8d79e8325fee39183fcf5e1d75
muhit-khan <muhitkhan_bd@outlook.com>
2024-01-04 09:08:11 +0530

    Refactor Services component to use ServicesData and MultiActionAreaCard

--- src/pages/Homepage/body/Services/Services.js
+++ src/pages/Homepage/body/Services/Services.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import "./Services.css"
-import { servicesData } from "./data"
-import CommonServices from '../../../../components/CommonServices/CommonServices'
+import { ServicesData } from "./ServicesData"
+import cardImage from './images/card.webp';
+import MultiActionAreaCard from '../../../../components/materialui/MultiActionAreaCard'
 
 const Services = () => {
     return (
-        <section className='home_service'>
-            <CommonServices data={servicesData} />
-            <button>More...</button>
-        </section>
+        <section className=''>
+            <h3 className='tilte'> Services </h3>
+            <div className='card-container'>
+                {ServicesData.map(service => (
+                    <MultiActionAreaCard
+                        key={service.id}
+                        imagePath={cardImage}
+                        title={service.title}
+                        description={service.description}
+                        buttonLink={service.link}
+                        buttonText="Explore"
+                    />
+                ))}
+            </div>
+        </section >
     )
 }
 
