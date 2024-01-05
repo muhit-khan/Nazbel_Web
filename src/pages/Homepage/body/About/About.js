eed3a0d01e8f5b9a6e1f0d8575fd06de67c03e7a
muhit-khan <muhitkhan_bd@outlook.com>
2024-01-05 12:07:26 +0530

    Refactor About component to include additional content

--- src/pages/Homepage/body/About/About.js
+++ src/pages/Homepage/body/About/About.js
@@ -3,7 +3,15 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about'>
+      <div className='photo'></div>
+      <div className='text-container'>
+        <h3 className='title'>About</h3>
+        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div className='ratings'></div>
+      <div className='button'></div>
+    </section>
   )
 }
 
