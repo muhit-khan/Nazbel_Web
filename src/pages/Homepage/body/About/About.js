8387bfffb588588a554b755b43ece696da97c3a1
muhit-khan <muhitkhan_bd@outlook.com>
2024-01-05 21:51:13 +0530

    Refactor About component and add AboutData

--- src/pages/Homepage/body/About/About.js
+++ src/pages/Homepage/body/About/About.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import "./About.css";
+import PhotoFrame from '../../../../components/PhotoFrame/PhotoFrame';
+import photoPath from './images/about-photo.webp';
+import Button from '../../../../components/Button/Button';
+import { AboutDescription, AboutSubtitle, ratingsData } from './AboutData';
+import Ratings from '../../../../components/Ratings/Ratings';
 
 const About = () => {
   return (
     <section className='about'>
-      <div className='photo'></div>
-      <div className='text-container'>
-        <h3 className='title'>About</h3>
-        <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className='about-text-container'>
+        <h3 className='about-title'>About</h3>
+        <p className='about-subtitle'>{AboutSubtitle}</p>
       </div>
-      <div className='ratings'></div>
-      <div className='button'></div>
+      <div className='about-photo-description'>
+        <div className='about-photo'>
+          <PhotoFrame imagePath={photoPath} />
+        </div>
+        <p className='about-description'>{AboutDescription}</p>
+      </div>
+      <div className='about-ratings'>
+        <Ratings ratingsData={ratingsData} />
+      </div>
+      <div className='about-button'>
+        <Button label="Know More" />
+      </div>
     </section>
   )
 }
 
-export default About
+export default About;
