fe148d2a6a68fcd269cafc7c3048198def8ae936
mehedih20 <mehedih20@gmail.com>
2024-01-07 17:55:31 +0530

    our group websites done

--- src/pages/Homepage/body/About/AboutData.js
+++
@@ -1,6 +0,0 @@
-export const ratingsData = [
-  { name: "Nazbel Fire", rating: 4.5 },
-  { name: "Nazbel Geotechnical", rating: 4.5 },
-  { name: "Nazbel Defence", rating: 4.5 },
-  { name: "Nazbel Robotice", rating: 4.5 },
-];
--- src/pages/Homepage/body/GroupWebsites/GroupWebsites.js
+++ src/pages/Homepage/body/GroupWebsites/GroupWebsites.js
@@ -1,11 +1,43 @@
 import React from "react";
 import "./GroupWebsites.css";
+import { FaFire, FaRobot } from "react-icons/fa";
+import { FaEarthAsia } from "react-icons/fa6";
+import { GiArmorUpgrade } from "react-icons/gi";
 
 const GroupWebsites = () => {
+  const ratingsData = [
+    {
+      name: "Nazbel Fireproofing",
+      icon: <FaFire style={{ color: "#BE3144" }} />,
+    },
+    {
+      name: "Nazbel Geotechnical",
+      icon: <FaEarthAsia style={{ color: "#0766AD" }} />,
+    },
+    {
+      name: "Nazbel Defence",
+      icon: <GiArmorUpgrade style={{ color: "#EC8F5E" }} />,
+    },
+    { name: "Nazbel Robotice", icon: <FaRobot style={{ color: "#C3ACD0" }} /> },
+  ];
+
   return (
     <section className="group-websites">
       <div className="container">
-        <h1 className="title-middle">GroupWebsites</h1>
+        <h1 className="title-middle">Our Group Websites</h1>
+
+        <div className="website-items">
+          {ratingsData.map((item, index) => {
+            const { name, icon } = item;
+
+            return (
+              <div key={index} className="website-item">
+                <h2>{icon}</h2>
+                <a href="">{name}</a>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
